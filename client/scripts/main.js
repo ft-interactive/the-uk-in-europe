c42e04b372ae01eda291c394c3c437fd8091e9e5
David Blood <davidbjourno@users.noreply.github.com>
2016-06-10 18:57:42 +0100

    Add basic quiz functionality

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,1 +1,26 @@
-// YOUR CODE HERE
+// TODO: flag UK circle on click
+// TODO: add splash screen for correct/incorrect
+// TODO: add custom GA event tracking
+
+const app = angular.module('app', ['ngAnimate']);
+
+app.controller('GfxCtrl', ['$scope', '$window', '$animate', function ($scope, $window, $animate) {
+  $scope.hover = {};
+  $scope.quizOver = false;
+
+  $scope.isGroup8 = () => {
+    console.log('Group 8');
+
+    $scope.quizOver = true;
+    $scope.answeredHeading = 'Heading that shows if you get it RIGHT!';
+    $window.ga('send', 'event', 'Responses', 'submit', 1);
+  };
+
+  $scope.isGroupN = () => {
+    console.log('Not group 8');
+
+    $scope.quizOver = true;
+    $scope.answeredHeading = 'Heading that shows if you get it WRONG!';
+    $window.ga('send', 'event', 'Responses', 'submit', 0);
+  };
+}]);
